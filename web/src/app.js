3a1127ca1d70d29997311e6e27f5478194391b27
feketejanosdavid <janosdavidfekete@gmail.com>
2024-04-16 10:16:56 +0200

    push

--- web/src/app.js
+++ web/src/app.js
@@ -5,7 +5,23 @@ const doc = {
 function getEmployees() {
     const url = 'http://localhost:8000/employees'
 
-    fetch(url).then(response => response.json())
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {renderEmployees(result)})
+}
+
+function renderEmployees(employeeList) {
+    employeeList.forEach(emp => {
+        console.log(emp.name)
+        var row = document.createElement('tr')
+        row.innerHTML = `
+        <td>${emp.id}</td>
+        <td>${emp.name}</td>
+        <td>${emp.city}</td>
+        <td>${emp.salary}</td>
+    `
+    doc.empBody.appendChild(row)
+    });
 }
 
 getEmployees()
