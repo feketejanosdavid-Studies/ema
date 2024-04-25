6c93cee166388ed5c9d12d4cf613aaead8b7c9f2
feketejanosdavid <janosdavidfekete@gmail.com>
2024-04-25 10:46:13 +0200

    3/4-e kész

--- web/src/app.js
+++ web/src/app.js
@@ -3,19 +3,24 @@ const doc = {
     addButton: document.getElementById('addButton'),
     nameDoc: document.getElementById('name'),
     cityDoc: document.getElementById('city'), 
-    salaryDoc: document.getElementById('salary')
+    salaryDoc: document.getElementById('salary'),
+    idDoc: document.getElementById('id')
 } 
 
 const state = {
     url: 'http://localhost:8000/employees',
     name: '',
     city: '',
-    salary:0
+    salary:0,
+    add:true
 }
 
 doc.addButton.addEventListener('click', () => {
-    console.log('Működik')
-    createEmployee()
+    console.log('Működik');
+    getDataFromForm();
+    createEmployee();
+    deleteEmployee();
+    getEmployees();
 })
 
 function getDataFromForm() {
@@ -38,17 +43,25 @@ function createEmployee() {
     })
 }
 
-function editEmployee(){
-    fetch(state.url, {
-        method: 'DELETE',
-
-    })
+function deleteEmployee() {
+    doc.idDoc.value="";
+    doc.nameDoc.value="";
+    doc.cityDoc.value="";
+    doc.salaryDoc.value="";
 }
+
+function clearTableContent() {
+    doc.empBody.textContent = "";
+}
+
 
 function getEmployees() {
     fetch(state.url)
     .then(response => response.json())
-    .then(result => {renderEmployees(result)})
+    .then(result => {
+        clearTableContent();
+        renderEmployees(result);
+    })
 }
 
 function renderEmployees(employeeList) {
@@ -60,13 +73,48 @@ function renderEmployees(employeeList) {
         <td>${emp.name}</td>
         <td>${emp.city}</td>
         <td>${emp.salary}</td>
-        <td>
-            <button class="btn btn-outline-primary">Szerkesztés</button>
-            <button class="btn btn-outline-danger">Törlés</button>
+        <td class="width text-center">
+            <button class="btn btn-outline-primary"
+            data-id="${emp.id}"
+            data-name="${emp.name}"
+            data-city="${emp.city}"
+            data-salary="${emp.salary}"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+            onclick="startEdit(this)"><i class="bi bi-pencil"></i></button>
+            <button class="btn btn-outline-danger" onclick="startDelete(${emp.id})"><i class="bi bi-trash3"></i></button>
         </td>
     `
     doc.empBody.appendChild(row)
     });
 }
+
+function startAdding() {
+    deleteEmployee();
+}
+
+function startDelete(id) {
+    console.log('törlődő elem:', id)
+    finishDelete(id)
+    getEmployees()
+}
+
+function finishDelete(id) {
+    let newUrl = state.url + '/' + id
+    fetch(newUrl, {
+        method: 'DELETE'
+    })
+}
+
+function startEdit(source) {
+    console.log("Szerkesztés...")
+    console.log(source.dataset.id)
+    doc.idDoc.value = source.dataset.id
+    doc.nameDoc.value = source.dataset.name
+    doc.cityDoc.value = source.dataset.city
+    doc.salaryDoc.value = source.dataset.salary
+}
+
+
 
 getEmployees()
